ec274066ecc34034df5f0e4fb429b9649e4f7df1
kayalvizhi-jeyabharathi <kayaljvizhi@gmail.com>
2021-05-28 17:31:06 +0530

    integrated

--- core/src/main/java/com/mindtree/test/core/models/AuthorImpl.java
+++ core/src/main/java/com/mindtree/test/core/models/AuthorImpl.java
@@ -1,5 +1,10 @@
 package com.mindtree.test.core.models;
 
+import javax.annotation.Resource;
+
+import org.apache.sling.models.annotations.Model;
+
+@Model(adaptables=Resource.class,adapter=Author.class)
 public class AuthorImpl {
 
 }
